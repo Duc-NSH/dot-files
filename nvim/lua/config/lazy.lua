a731ebfcb5c90401aa5252780f66223f2925c8a7
Duc-NSH <ducnsh.hust@gmail.com>
2025-02-12 15:23:02 +0700

    Add auto reload buffers

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,7 +12,15 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
         os.exit(1)
     end
 end
+
 vim.opt.rtp:prepend(lazypath)
+
+-- auto-reload files when modified externally
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+    command = "if mode() != 'c' | checktime | endif",
+    pattern = { "*" },
+})
 
 require("lazy").setup({
     spec = {
