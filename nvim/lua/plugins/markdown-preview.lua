41cb51f3fbee0e95071213bb1a09489a57596eb8
Duc-NSH <ducnsh.hust@gmail.com>
2026-06-11 15:59:54 +0700

    add plugins config

---
+++ nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,11 @@
+return {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+        vim.fn["mkdp#util#install"]()
+    end,
+    init = function()
+        vim.g.mkdp_theme = "light"
+    end,
+}
